5744fd4ed8ecace24094373bec63c9d55423de7b
Joyce Yang <jcyang@hmc.edu>
2017-06-20 09:29:36 -0400

    First commit

---
+++ src/main/java/byjc/ATM.java
@@ -0,0 +1,12 @@
+package byjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATM {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ATM.class, args);
+	}
+}
---
+++ src/test/java/byjc/ATMTests.java
@@ -0,0 +1,16 @@
+package byjc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ATMTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
